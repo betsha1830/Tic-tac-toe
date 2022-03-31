42a837082be5c3f245d787e2b5fad17bb3a8dba2
Betsue Weldemariam <btsegay0@gmail.com>
2022-03-31 23:01:40 +0300

    Checking of rows completed.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -16,25 +16,63 @@
 
 class TicTacToe
 
-    board = [[],[],[]]
-    turn = 1
-    shape = "O"
-    game_end = true
+    def initialize
+
+        @board = [["","",""],["O","O",""],["","",""]]
+        @turn = 1
+        @shape = "O"
+        @game_end = true
+
+    end
 
     def playing_board
-       puts "\t 1     2     3"
-       puts "    1 |  #{board[0][0]}  |  #{board[0][1]}  |  #{board[0][2]}  |"
-       puts "_________________________"
-       puts "    2 |  #{board[1][0]}  |  #{board[1][1]}  |  #{board[1][2]}  |"
-       puts "_________________________"
-       puts "    3 |  #{board[2][0]}  |  #{board[2][1]}  |  #{board[2][2]}  |"
+        puts "\t 1     2     3"
+        puts "    1 |  #{@board[0][0]}  |  #{@board[0][1]}  |  #{@board[0][2]}  |"
+        puts "    _____________________"
+        puts "    2 |  #{@board[1][0]}  |  #{@board[1][1]}  |  #{@board[1][2]}  |"
+        puts "    _____________________"
+        puts "    3 |  #{@board[2][0]}  |  #{@board[2][1]}  |  #{@board[2][2]}  |"
+    end
+
+    def move
+        ((@turn%2).even?) ? @shape = "X" : @shape = "O"
+        puts "It's #{@shape}'s turn."
+        input = gets.chomp
+        temp_arr = input.split("")
+        input_position = []
+        temp_arr.each { |item| input_position.push(item.to_i) }
+        temp_arr = []
+        until @board[input_position[0]][input_position[1]] == ""
+            puts "Position already taken. Input another field."
+            input = gets.chomp
+            temp_arr = input.split("")
+            input_position = []
+            temp_arr.each { |item| input_position.push(item.to_i) }
+            temp_arr = []
+        end
+        @board[input_position[0]][input_position[1]] = @shape
+        @turn += 1
+    end
+
+    def check_game
+
+        @board.each do |item|
+
+            (item.all?(@shape)) ? (puts "#{@shape} wins."; @game_end = true) : ()
+
+
+        end
+
     end
 
     def play_game
 
         game_end = false
 
-
     end
 
 end
+
+x = TicTacToe.new()
+puts x.move
+puts x.check_game
