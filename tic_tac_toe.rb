a787cfa2867ca7a2b29d72edca654ba354027156
Betsue Weldemariam <btsegay0@gmail.com>
2022-04-01 10:07:43 +0300

    check_game function checks all possible winning scenarios.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 # PROBLEM
 # Create a TicTacToe game
 # (Consists of two players taking turn, a playing board, filling out the board in a number of ways to win the game)
@@ -15,64 +16,72 @@
 #   2.4- Prompt user if they want to play again.
 
 class TicTacToe
+  def initialize
+    @board = [['', '', ''], ['', '', ''], ['', '', '']]
+    @turn = 1
+    @shape = 'O'
+    @game_end = true
+  end
 
-    def initialize
+  def playing_board
+    puts "\t 1     2     3"
+    puts "    1 |  #{@board[0][0]}  |  #{@board[0][1]}  |  #{@board[0][2]}  |"
+    puts '    _____________________'
+    puts "    2 |  #{@board[1][0]}  |  #{@board[1][1]}  |  #{@board[1][2]}  |"
+    puts '    _____________________'
+    puts "    3 |  #{@board[2][0]}  |  #{@board[2][1]}  |  #{@board[2][2]}  |"
+  end
 
-        @board = [["","",""],["O","O",""],["","",""]]
-        @turn = 1
-        @shape = "O"
-        @game_end = true
-
+  def move
+    @shape = (@turn % 2).even? ? 'X' : 'O'
+    puts "It's #{@shape}'s turn."
+    input = gets.chomp
+    temp_arr = input.split('')
+    input_position = []
+    temp_arr.each { |item| input_position.push(item.to_i) }
+    temp_arr = []
+    until @board[input_position[0]][input_position[1]] == ''
+      puts 'Position already taken. Input another field.'
+      input = gets.chomp
+      temp_arr = input.split('')
+      input_position = []
+      temp_arr.each { |item| input_position.push(item.to_i) }
+      temp_arr = []
     end
+    @board[input_position[0]][input_position[1]] = @shape
+    @turn += 1
+  end
 
-    def playing_board
-        puts "\t 1     2     3"
-        puts "    1 |  #{@board[0][0]}  |  #{@board[0][1]}  |  #{@board[0][2]}  |"
-        puts "    _____________________"
-        puts "    2 |  #{@board[1][0]}  |  #{@board[1][1]}  |  #{@board[1][2]}  |"
-        puts "    _____________________"
-        puts "    3 |  #{@board[2][0]}  |  #{@board[2][1]}  |  #{@board[2][2]}  |"
+  def check_game
+    @board.each do |item|
+      if item.all?(@shape)
+        puts "#{@shape} wins."
+      end
+      @game_end = true
     end
-
-    def move
-        ((@turn%2).even?) ? @shape = "X" : @shape = "O"
-        puts "It's #{@shape}'s turn."
-        input = gets.chomp
-        temp_arr = input.split("")
-        input_position = []
-        temp_arr.each { |item| input_position.push(item.to_i) }
-        temp_arr = []
-        until @board[input_position[0]][input_position[1]] == ""
-            puts "Position already taken. Input another field."
-            input = gets.chomp
-            temp_arr = input.split("")
-            input_position = []
-            temp_arr.each { |item| input_position.push(item.to_i) }
-            temp_arr = []
-        end
-        @board[input_position[0]][input_position[1]] = @shape
-        @turn += 1
+    if ((@board[0][0] == @board[1][0]) && @board[0][0] == @board[2][0])
+        game_end = true
+        puts "#{@shape} wins."
+    elsif ((@board[0][1] == @board[1][1]) && @board[0][1] == @board[2][1])
+        game_end = true
+        puts "#{@shape} wins."
+    elsif ((@board[0][2] == @board[1][2]) && @board[0][2] == @board[2][2])
+        game_end = true
+        puts "#{@shape} wins."
+    elsif ((@board[0][0] == @board[1][1]) && @board[0][0] == @board[2][2])
+        game_end = true
+        puts "#{@shape} wins."
+    elsif ((@board[1][2] == @board[1][1]) && @board[1][2] == @board[2][0])
+        game_end = true
+        puts "#{@shape} wins."
     end
+  end
 
-    def check_game
-
-        @board.each do |item|
-
-            (item.all?(@shape)) ? (puts "#{@shape} wins."; @game_end = true) : ()
-
-
-        end
-
-    end
-
-    def play_game
-
-        game_end = false
-
-    end
-
+  def play_game
+    game_end = false
+  end
 end
 
-x = TicTacToe.new()
+x = TicTacToe.new
 puts x.move
 puts x.check_game
